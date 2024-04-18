a8246e1a0dad311b02a5fdca9f6955c9596d0db5
Roman <romalupan89@gmail.com>
2024-04-18 13:42:33 +0300

    Add project files.

---
+++ WebAPICourse/Models/Pokemon.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPICourse.Models
+{
+    public class Pokemon
+    {
+        // This is a modal-A class with bunch of props in it
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
