f26e84d39fbf9c893d6aca766aa05d7e1e39d6a4
Roman <romalupan89@gmail.com>
2024-04-18 21:15:07 +0300

    many to many

--- WebAPICourse/Models/Pokemon.cs
+++ WebAPICourse/Models/Pokemon.cs
@@ -8,6 +8,8 @@
         public DateTime BirthDate { get; set; }
 
         //This is a one to many relation in SQL
-        public ICollection<Review> Reviews { get; set; }     
+        public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
     }
 }
---
+++ WebAPICourse/Models/PokemonCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPICourse.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+        public int CatrgoryId { get; set; }
+        public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
+
+
+    }
+}
